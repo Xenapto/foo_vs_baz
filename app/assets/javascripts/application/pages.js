dc9a733de8a3b323b7b19e3a17dfab142a9876dd
Jeremy Green <jeremy@octolabs.com>
2012-12-17 18:23:41 -0600

    Make sure the fanciness works when the form is reshown if the page isn't valid.

--- app/assets/javascripts/application/pages.js
+++ app/assets/javascripts/application/pages.js
@@ -2,9 +2,13 @@ var FooVsBaz = FooVsBaz || {};
 FooVsBaz.pages = FooVsBaz.pages || {};
 FooVsBaz.pages_new = FooVsBaz.pages_new || {};
 FooVsBaz.pages_edit = FooVsBaz.pages_edit || {};
+FooVsBaz.pages_create = FooVsBaz.pages_create || {};
+FooVsBaz.pages_update = FooVsBaz.pages_update || {};
 
 FooVsBaz.pages_new.init =
   FooVsBaz.pages_new.init =
+  FooVsBaz.pages_create.init =
+  FooVsBaz.pages_update.init =
   function(){ FooVsBaz.pages.form_init(); }
 
 FooVsBaz.pages.form_init = function(){
