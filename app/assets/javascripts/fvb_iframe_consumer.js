cec1e1e1a46eaf4340b4ca8c63025266e10dcd3c
Chris Handley <github.20.alabama3@spamgourmet.com>
2015-03-20 11:52:48 +0000

    Added script to load Artoo into consumer
    this is done in the consumer.js javascript

--- app/assets/javascripts/fvb_iframe_consumer.js
+++ app/assets/javascripts/fvb_iframe_consumer.js
@@ -1,7 +1,9 @@
 //= require easymarklet/consumer
 //= require fvb_iframe_bookmarklet
+//= require artoo.js
 //
 var fvb_iframe_consumer = new Easymarklet.Consumer(FvbIframeBookmarklet);
 fvb_iframe_consumer.consumerMethod = function(){console.log('consumer defined iframe_consumer.js')};
 fvb_iframe_consumer.init();
+loadArtoo();
 
